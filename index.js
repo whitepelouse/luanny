e441197bbe09b5a05f2949612d957cb9946b13ca
whitepelouse <46036091+whitepelouse@users.noreply.github.com>
2018-12-21 00:13:28 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const http = require('http');  
+
+ 
+ 
+(async () => { 
+  const requestHandler = (request, response) => {  
+       console.log(request.url)
+       response.end('Hello Worldccc linda2 ')
+  }
+       
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+ 
+})();
+
+const puppeteer = require('puppeteer');
+function kiusi(){
+  (async () => {
+  console.log('-----2222 kiusi00000-----------');
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  
+  const page = await browser.newPage();
+  await page.goto('https://tuhithaihau.github.io/site/');  
+     
+  console.log('----222 XONG0-222-------');
+  setTimeout(async () => await await page.close(), 290000);
+  setTimeout(async () => await browser.close(), 295000);
+  console.log('----222 XONG--------');
+  
+  })();
+}
+
+function f() { 
+    console.log('-222222 .  111111111111-------trong f -----------');
+    kiusi();
+    console.log(Date()); 
+    setTimeout( f, 300000 ); 
+    console.log('--222222 .  tttttttttt--') 
+}
+
+
+
+f();
+
+
